3714cffffa316c28c805d997202c79afff8c2729
jatinkumar1985 <jatin.kumar@jagrannewmedia.com>
2026-02-09 12:31:46 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,40 @@
 /** @type {import('next').NextConfig} */
+
+const appEnv = process.env.APP_ENV || "development";
+import loadEnv from "./env-loader.js";
+loadEnv(appEnv);
+
 const nextConfig = {
-  /* config options here */
+    images: {
+        unoptimized: true,
+        minimumCacheTTL: 10,
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "imgeng.jagran.com",
+                port: "",
+                pathname: "/**",
+            }
+        ],
+    },
+    async rewrites() {
+        return [
+            
+        ];
+    },
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "no-store, must-revalidate",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
