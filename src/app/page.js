60d75935a4e615e3a54010fe6e0baf8f1719d9e8
jatinkumar1985 <jatin.kumar@jagrannewmedia.com>
2026-02-13 11:23:26 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,3 @@
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 import PromoBanner from "@/component/global/PromoBanner";
 import LatestNews from "@/component/home/LatestNews";
 import { LatestArticleService, PromoBannerService } from "@/services/ListingService";
